fb44bb891b5b33f72660e5d242c889a96719b5b4
Nicole Arroyo <nicole.m.arroyo@gmail.com>
2023-01-15 11:50:41 -0600

    "Week 1 Assignment"

--- server.js
+++ server.js
@@ -1,6 +1,9 @@
 const express = require('express');
+const res = require('express/lib/response');
 const morgan = require('morgan');
 const campsiteRouter = require('./routes/campsiteRouter');
+const promotionRouter = require('./routes/promotionRouter');
+const partnerRouter = require('./routes/partnerRouter');
 
 const hostname = 'localhost';
 const port = 3000;
@@ -8,6 +11,10 @@ const port = 3000;
 const app = express();
 app.use(morgan('dev'));
 app.use(express.json());
+
+app.use('/campsites', campsiteRouter);
+app.use('/promotions', promotionRouter);
+app.use('/partners', partnerRouter);
 
 app.use(express.static(__dirname + '/public'));
 
